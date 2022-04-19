2084deccd02536d541d6db57240a1bc73d03e612
pokabook <khs0826@dsm.hs.kr>
2022-04-19 23:26:15 +0900

    자바 공부

---
+++ src/main/java/study/Chapter4/UtilDemo.java
@@ -0,0 +1,12 @@
+package study.Chapter4;
+
+class Util{
+    static int fourTiems(int i) {
+        return i * 4;
+    }
+}
+public class UtilDemo {
+    public static void main(String[] args) {
+        System.out.println(Util.fourTiems(5));
+    }
+}
