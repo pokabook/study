dde22eeeabb65fa3363c018fb81ef3ad9af42903
pokabook <khs0826@dsm.hs.kr>
2022-04-19 14:02:47 +0900

    자바 공부

---
+++ src/main/java/study/Chapter3/Switch1Demo.java
@@ -0,0 +1,10 @@
+package study.Chapter3;
+
+import java.util.Scanner;
+
+public class Switch1Demo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("등수를 입력하세여 : ");
+    }
+}
