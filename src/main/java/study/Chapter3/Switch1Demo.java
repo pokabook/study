deaf45411fa0f5d73f15b3de0f78f293e35569b8
pokabook <khs0826@dsm.hs.kr>
2022-04-19 18:07:39 +0900

    자바 공부

--- src/main/java/study/Chapter3/Switch1Demo.java
+++ src/main/java/study/Chapter3/Switch1Demo.java
@@ -6,5 +6,28 @@ public class Switch1Demo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("등수를 입력하세여 : ");
+        int rank = in.nextInt();
+
+        switch (rank){
+            case 1:
+                System.out.println("일등입니다.");
+                break;
+            case 2:
+                System.out.println("이등입니다.");
+                break;
+            case 3:
+                System.out.println("삼등입니다.");
+                break;
+            default:
+                System.out.println("등외입니다.");
+        }
+
+        /*switch (rank) {
+            case 1 -> System.out.println("일등입니다.");
+            case 2 -> System.out.println("이등입니다.");
+            case 3 -> System.out.println("삼등입니다.");
+            default -> System.out.println("등외입니다.");
+        }*/
+
     }
 }
